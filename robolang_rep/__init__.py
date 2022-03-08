e907879a9fd3b23133620997636251150c912870
Suraj Nair <surajn@devfair0210.h2.fair>
2022-03-07 21:36:44 -0800

    cleanup / easier model loading

--- robolang_rep/__init__.py
+++ robolang_rep/__init__.py
@@ -1,1 +1,75 @@
 from robolang_rep.models_r3m import R3M
+
+import os 
+from os.path import expanduser
+import omegaconf
+import hydra
+import gdown
+import torch
+
+def load_r3m(modelid):
+    home = os.path.join(expanduser("~"), ".r3m")
+    if modelid == "resnet50":
+        foldername = "r3m_50"
+        modelurl = 'https://drive.google.com/uc?id=1Xu0ssuG0N1zjZS54wmWzJ7-nb0-7XzbA'
+        configurl = 'https://drive.google.com/uc?id=10jY2VxrrhfOdNPmsFdES568hjjIoBJx8'
+    elif modelid == "resnet34":
+        foldername = "r3m_34"
+        modelurl = 'https://drive.google.com/uc?id=15bXD3QRhspIRacOKyWPw5y2HpoWUCEnE'
+        configurl = 'https://drive.google.com/uc?id=1RY0NS-Tl4G7M1Ik_lOym0b5VIBxX9dqW'
+    elif modelid == "resnet18":
+        foldername = "r3m_18"
+        modelurl = 'https://drive.google.com/uc?id=1A1ic-p4KtYlKXdXHcV2QV0cUzI4kn0u-'
+        configurl = 'https://drive.google.com/uc?id=1nitbHQ-GRorxc7vMUiEHjHWP5N11Jvc6'
+    else:
+        raise NameError('Invalid Model ID')
+
+    if not os.path.exists(os.path.join(home, foldername)):
+        os.makedirs(os.path.join(home, foldername))
+    modelpath = os.path.join(home, foldername, "model.pt")
+    configpath = os.path.join(home, foldername, "config.yaml")
+    if not os.path.exists(modelpath):
+        gdown.download(modelurl, modelpath, quiet=False)
+        gdown.download(configurl, configpath, quiet=False)
+
+    modelcfg = omegaconf.OmegaConf.load(configpath)
+    rep = hydra.utils.instantiate(modelcfg.agent)
+    rep = torch.nn.DataParallel(rep)
+    rep.load_state_dict(torch.load(modelpath)['r3m'])
+    return rep
+
+def load_r3m_reproduce(modelid):
+    home = os.path.join(expanduser("~"), ".r3m")
+    if modelid == "r3m":
+        foldername = "original_r3m"
+        modelurl = 'https://drive.google.com/uc?id=1jLb1yldIMfAcGVwYojSQmMpmRM7vqjp9'
+        configurl = 'https://drive.google.com/uc?id=1cu-Pb33qcfAieRIUptNlG1AQIMZlAI-q'
+    elif modelid == "r3m_noaug":
+        foldername = "original_r3m_noaug"
+        modelurl = 'https://drive.google.com/uc?id=1k_ZlVtvlktoYLtBcfD0aVFnrZcyCNS9D'
+        configurl = 'https://drive.google.com/uc?id=1hPmJwDiWPkd6GGez6ywSC7UOTIX7NgeS'
+    elif modelif == "r3m_nol1":
+        foldername = "original_r3m_nol1"
+        modelurl = 'https://drive.google.com/uc?id=1LpW3aBMdjoXsjYlkaDnvwx7q22myM_nB'
+        configurl = 'https://drive.google.com/uc?id=1rZUBrYJZvlF1ReFwRidZsH7-xe7csvab'
+    elif modelif == "r3m_nolang":
+        foldername = "original_r3m_nolang"
+        modelurl = 'https://drive.google.com/uc?id=1FXcniRei2JDaGMJJ_KlVxHaLy0Fs_caV'
+        configurl = 'https://drive.google.com/uc?id=192G4UkcNJO4EKN46ECujMcH0AQVhnyQe'
+    else:
+        raise NameError('Invalid Model ID')
+
+    if not os.path.exists(os.path.join(home, foldername)):
+        os.makedirs(os.path.join(home, foldername))
+    modelpath = os.path.join(home, foldername, "model.pt")
+    configpath = os.path.join(home, foldername, "config.yaml")
+    if not os.path.exists(modelpath):
+        gdown.download(modelurl, modelpath, quiet=False)
+        gdown.download(configurl, configpath, quiet=False)
+
+    modelcfg = omegaconf.OmegaConf.load(configpath)
+    rep = hydra.utils.instantiate(modelcfg.agent)
+    rep = torch.nn.DataParallel(rep)
+    rep.load_state_dict(torch.load(modelpath)['r3m'])
+    return rep
+    
--- robolang_rep/example.py
+++ robolang_rep/example.py
@@ -5,21 +5,13 @@ import torchvision.transforms as T
 import numpy as np
 from PIL import Image
 
-load_path = "/checkpoint/surajn/drqoutput/train_representation/2022-01-24_11-10-28/10_agent.finetunelang=0,agent.l1weight=1e-05,agent.langtype=lorel,agent.langweight=1.0,agent.size=50,batch_size=16,dataset=ego4d,doaug=rctraj,experiment=rep_0124/snapshot_1000000.pt"
+from robolang_rep import load_r3m
 
 device = "cuda"
 
-## READ CONFIG AND INSTANTIATE MODEL
-parent_dir = "/".join(load_path.split("/")[:-1])
-hydra_path = parent_dir + "/.hydra/config.yaml"
-modelcfg = omegaconf.OmegaConf.load(hydra_path)
-rep = hydra.utils.instantiate(modelcfg.agent)
-rep = torch.nn.DataParallel(rep)
-
-## LOAD WEIGHTS
-rep.load_state_dict(torch.load(load_path)['r3m'])
-rep.eval()
-rep.to(device)
+r3m = load_r3m("resnet50") # resnet18, resnet34
+r3m.eval()
+r3m.to(device)
 
 ## DEFINE PREPROCESSING
 transforms = T.Compose([T.Resize(256),
@@ -30,5 +22,5 @@ transforms = T.Compose([T.Resize(256),
 image = np.random.randint(0, 255, (500, 500, 3))
 preprocessed_image = transforms(Image.fromarray(image.astype(np.uint8))).reshape(-1, 3, 224, 224)
 preprocessed_image.to(device) 
-embedding = rep(preprocessed_image * 255.0)[0] ## R3M expects image input to be [0-255]
+embedding = r3m(preprocessed_image * 255.0)[0] ## R3M expects image input to be [0-255]
 print(embedding.shape) # [1, 2048]
